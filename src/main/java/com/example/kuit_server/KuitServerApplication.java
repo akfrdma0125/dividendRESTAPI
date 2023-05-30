56906601102383e5bf56c2a3a364486dc602a69a
akfrdma0125 <nogain1215@gmail.com>
2023-05-30 17:52:35 +0900

    inital commit

---
+++ src/main/java/com/example/kuit_server/KuitServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.kuit_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuitServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuitServerApplication.class, args);
+    }
+
+}
